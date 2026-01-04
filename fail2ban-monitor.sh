e7daf4616de45661003bf7873149e3bacb167007
Hitsukaya Valentaizar <87619517+Hitsukaya@users.noreply.github.com>
2026-01-04 22:11:32 +0200

    Add Fail2Ban monitoring script

---
+++ fail2ban-monitor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Fail2Ban Monitoring
+
+echo "===== FAIL2BAN MONITOR ====="
+
+fail2ban-client status sshd
+fail2ban-client status postgresql
+fail2ban-client status nginx-http-auth
+fail2ban-client status laravel-scan
+fail2ban-client status nginx-error
+fail2ban-client status nginx-blocked
+fail2ban-client status nginx-scanner
+fail2ban-client status nginx-ratelimit
+fail2ban-client status laravel-wordpress
+fail2ban-client status phpmyadmin
+fail2ban-client status recidive
+fail2ban-client status nginx-ssl-handshake-protection
+grep Ban /var/log/fail2ban.log | tail -n 5
+tail -n 10 /var/log/nginx/error.log
